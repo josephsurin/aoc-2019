ed9d998503c1cfd8587771cd77234e12545bc2d3
fumetsuu <joseph.surin@gmail.com>
2019-12-16 18:20:40 +1100

    day 16 sols

---
+++ day-16/day-16.py
@@ -0,0 +1,44 @@
+from gmpy2 import lcm, gcd
+inp = open('input').read().strip()
+
+# part 1
+base = [0,1,0,-1]
+def elongate(b, t):
+    o = []
+    for i in range(len(b)*t):
+        o.append(b[i//t])
+    return o
+bases = [elongate(base, i+1) for i in range(700)]
+
+def go_phase1(inp):
+    inp = [int(x) for x in inp]
+    o = ''
+    for p in range(len(inp)):
+        out = 0
+        for i,x in enumerate(inp):
+            # pattern = elongate(base, p+1)
+            pattern = bases[p]
+            out += x*pattern[(p-i-1)%len(pattern)]
+        o += str(abs(out)%10)
+    return o
+for _ in range(100):
+    inp = go_phase1(inp)
+print('part 1', inp[:8])
+
+# part 2
+inp = open('input').read().strip()*10000
+offset = int(inp[:7])
+inp = inp[offset:]
+def go_phase2(inp):
+    inp = [int(x) for x in inp]
+    return ''.join(str(sum(inp[i:])%10) for i in range(len(inp)))
+    o = ''
+    s = sum(inp)
+    o += str(s%10)
+    for x in inp:
+        o += str(abs(s-int(x))%10)
+    return o
+
+for _ in range(100):
+    inp = go_phase2(inp)
+print('part 2', inp[:8])
